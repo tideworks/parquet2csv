5d5f99d712d48b4e9e8b257dffcdf1126d13e48d
roger-dv <roger.dewayne@gmail.com>
2019-05-15 10:30:40 -0700

    updated run.sh to be able to invoke prq2csv program independent of build version of the jar file

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-HOME=${PWD}
-HADOOP_HOME=${PWD}
+if [[ "$OSTYPE" == 'msys' ]]; then
+   sep=';'
+else
+   sep=':'
+fi
 
-exec -a prq2csv java -ea -Dapp.name=prq2csv -jar target/parquet2csv-1.0-SNAPSHOT.jar "$@"
+dir=./target
+clspath=`find ${dir}/ -maxdepth 1 -type f -name "*.jar"|tr '\n' $sep`
+if [ -z ${CLASSPATH+x} ]; then
+  CLASSPATH=${clspath}
+else
+  CLASSPATH=${clspath}$sep${CLASSPATH}
+fi
+
+app_jar_file=
+for path in ${CLASSPATH//${sep}/ }; do
+#   echo "${path}"
+    [[ "${path}" =~ ^.*\/parquet2csv-.*\.jar$ ]] && CLASSPATH=${path}${sep}${CLASSPATH} && app_jar_file=${path} && break
+done
+
+export HOME=${PWD}
+export HADOOP_HOME=${PWD}
+
+exec -a prq2csv java -ea -Dapp.name=prq2csv -jar ${app_jar_file} "$@"
